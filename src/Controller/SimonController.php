c4cf2990f465b55eb0ee3d3f849badabaabc16f7
bdulong <baptistedulong@gmail.com>
2023-06-21 10:22:22 +0200

    euh ouais

---
+++ src/Controller/SimonController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SimonController extends AbstractController
+{
+    #[Route('/simon', name: 'app_simon')]
+    public function index(): Response
+    {
+        return $this->render('simon/index.html.twig', [
+            'controller_name' => 'SimonController',
+        ]);
+    }
+
+    #[Route('/michael', name: 'michael')]
+    public function index(): Response
+    {
+        return $this->render('simon/michael.html.twig', [
+            'controller_name' => 'michael',
+        ]);
+    }
+}
